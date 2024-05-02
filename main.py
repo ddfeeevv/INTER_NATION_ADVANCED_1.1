1cfe7e09340c34ab40c1ba1532700411355cd12b
Ваше Имя <your@example.co>
2024-05-02 23:14:13 +0500

    к сожалению, время выполнения около 5 секунд, требуется 1. конец

--- main.py
+++ main.py
@@ -91,85 +91,86 @@ screenshots = [
     {"input_path": "screenshot.png", "output_path": "cropped_third_russian.png", "width": 490, "height": 90, "x_offset": 30, "y_offset": 1106},
     {"input_path": "screenshot.png", "output_path": "cropped_fourth_russian.png", "width": 490, "height": 90, "x_offset": 30, "y_offset": 1214}
 ]
+for i in range(10):
+    take_screenshot("screenshot.png")
 
-take_screenshot("screenshot.png")
+    # Словарь английских слов и их переводов на русский
+    dictionary = {
+        "Tablecloth": "Скатерть",
+        "Hat": "Шляпа",
+        "Melon": "Дыня",
+        "Nurse": "Медсестра",
+        "CinnamonRoll": "Булочка с корицей",
+        "Brave": "Храбрый",
+        "Sofa": "Диван",
+        "Fried": "Жареный",
+        "Restaurant": "Ресторан",
+        "Watermelon": "Арбуз",
+        "Rooftop": "Крыша",
+        "Carpet": "Ковер",
+        "Platform": "Платформа",
+        "Goodmoming": "Доброе утро",
+    }
 
-# Словарь английских слов и их переводов на русский
-dictionary = {
-    "Tablecloth": "Скатерть",
-    "Hat": "Шляпа",
-    "Melon": "Дыня",
-    "Nurse": "Медсестра",
-    "CinnamonRoll": "Булочка с корицей",
-    "«Fridge": "Холодильник",
-    "Sofa": "Диван",
-    "Fried": "Жареный",
-    "Restaurant": "Ресторан",
-    "Watermelon": "Арбуз",
-    "Rooftop": "Крыша",
-    "Carpet": "Ковер",
-    "Platform": "Платформа",
-    "Goodmoming": "Доброе утро",
-}
+    # Список для хранения распознанных текстов на русском
+    russian_texts = []
 
-# Список для хранения распознанных текстов на русском
-russian_texts = []
-
-# Обрабатываем первый скриншот на английском языке
-crop_image(screenshots[0]["input_path"], screenshots[0]["output_path"], screenshots[0]["width"], screenshots[0]["height"], screenshots[0]["x_offset"], screenshots[0]["y_offset"])
-img = cv2.imread(screenshots[0]["output_path"])
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-# Обрабатываем первый скриншот на английском языке
-text_eng = process_first_screenshot(screenshots[0]["output_path"])
-
-# Обрабатываем остальные скриншоты на русском языке
-for screenshot in screenshots[1:]:
-    crop_image(screenshot["input_path"], screenshot["output_path"], screenshot["width"], screenshot["height"], screenshot["x_offset"], screenshot["y_offset"])
-    img = cv2.imread(screenshot["output_path"])
+    # Обрабатываем первый скриншот на английском языке
+    crop_image(screenshots[0]["input_path"], screenshots[0]["output_path"], screenshots[0]["width"], screenshots[0]["height"], screenshots[0]["x_offset"], screenshots[0]["y_offset"])
+    img = cv2.imread(screenshots[0]["output_path"])
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    text_rus = pytesseract.image_to_string(img, lang='rus').strip()
-    russian_texts.append(text_rus)
 
-# Функция для поиска индекса перевода в массиве russian_texts
-def find_translation(word, russian_words):
-    if word in dictionary:
-        translation = dictionary[word]
-        for index, russian_word in enumerate(russian_words):
-            seq = difflib.SequenceMatcher(None, translation.lower(), russian_word.lower())
-            ratio = seq.ratio()
-            if ratio > 0.8:  # Пороговое значение сходства строк
-                return index
-    return None
+    # Обрабатываем первый скриншот на английском языке
+    text_eng = process_first_screenshot(screenshots[0]["output_path"])
 
-# Проверяем перевод слова из словаря и нажимаем соответствующие координаты
-translation_index = find_translation(text_eng, russian_texts)
+    # Обрабатываем остальные скриншоты на русском языке
+    for screenshot in screenshots[1:]:
+        crop_image(screenshot["input_path"], screenshot["output_path"], screenshot["width"], screenshot["height"], screenshot["x_offset"], screenshot["y_offset"])
+        img = cv2.imread(screenshot["output_path"])
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        text_rus = pytesseract.image_to_string(img, lang='rus').strip()
+        russian_texts.append(text_rus)
 
-if translation_index is not None:
-    if translation_index == 0:
+    # Функция для поиска индекса перевода в массиве russian_texts
+    def find_translation(word, russian_words):
+        if word in dictionary:
+            translation = dictionary[word]
+            for index, russian_word in enumerate(russian_words):
+                seq = difflib.SequenceMatcher(None, translation.lower(), russian_word.lower())
+                ratio = seq.ratio()
+                if ratio > 0.8:  # Пороговое значение сходства строк
+                    return index
+        return None
+
+    # Проверяем перевод слова из словаря и нажимаем соответствующие координаты
+    translation_index = find_translation(text_eng, russian_texts)
+
+    if translation_index is not None:
+        if translation_index == 0:
+            run_adb_command('adb -s emulator-5554 shell input tap 540 1600')
+            time.sleep(0.1)
+            run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
+        elif translation_index == 1:
+            run_adb_command('adb -s emulator-5554 shell input tap 540 1800')
+            time.sleep(0.1)
+            run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
+        elif translation_index == 2:
+            run_adb_command('adb -s emulator-5554 shell input tap 540 2000')
+            time.sleep(0.1)
+            run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
+        elif translation_index == 3:
+            run_adb_command('adb -s emulator-5554 shell input tap 540 2200')
+            time.sleep(0.1)
+            run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
+    else:
+        print("Перевод не найден")
         run_adb_command('adb -s emulator-5554 shell input tap 540 1600')
         time.sleep(0.1)
         run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
-    elif translation_index == 1:
-        run_adb_command('adb -s emulator-5554 shell input tap 540 1800')
-        time.sleep(0.1)
-        run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
-    elif translation_index == 2:
-        run_adb_command('adb -s emulator-5554 shell input tap 540 2000')
-        time.sleep(0.1)
-        run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
-    elif translation_index == 3:
-        run_adb_command('adb -s emulator-5554 shell input tap 540 2200')
-        time.sleep(0.1)
-        run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
-else:
-    print("Перевод не найден")
-    run_adb_command('adb -s emulator-5554 shell input tap 540 1600')
-    time.sleep(0.1)
-    run_adb_command('adb -s emulator-5554 shell input tap 540 2500')
-    save_failed_screenshot(text_eng, screenshot_path, text_eng)
+        save_failed_screenshot(text_eng, screenshot_path, text_eng)
 
-end_time = time.time()
+    end_time = time.time()
 
-execution_time = end_time - start_time
-print(f"Время выполнения скрипта: {execution_time} секунд")
+    execution_time = end_time - start_time
+    print(f"Время выполнения скрипта: {execution_time} секунд")
+print(1)
